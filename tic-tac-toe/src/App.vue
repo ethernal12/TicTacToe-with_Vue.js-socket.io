b25de762cd640676d825d2a804560dfe957212c9
Danijel <31479093+ethernal12@users.noreply.github.com>
2022-05-29 22:59:09 +0200

    Adding css styling.

--- tic-tac-toe/src/App.vue
+++ tic-tac-toe/src/App.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="container">
+    <div id="boarder">
+      <h1>Tic Tac Toe</h1>
+      <button id="start" @click="startgame()">Start game</button>
+      <h2>{{ displayTurn }}</h2>
 
-    <h1>Tic Tac Toe</h1>
-    <button id="start" @click="startgame()">Start game</button>
-    <h2>{{ displayTurn }}</h2>
+      <br>
+      <div class="play-area">
+        <div id="block_0" class="block" @click="draw(0), false">{{ content[0] }}</div>
+        <div id="block_1" class="block" @click="draw(1), false">{{ content[1] }}</div>
+        <div id="block_2" class="block" @click="draw(2), false">{{ content[2] }}</div>
+        <div id="block_3" class="block" @click="draw(3), false">{{ content[3] }}</div>
+        <div id="block_4" class="block" @click="draw(4), false">{{ content[4] }}</div>
+        <div id="block_5" class="block" @click="draw(5), false">{{ content[5] }}</div>
+        <div id="block_6" class="block" @click="draw(6), false">{{ content[6] }}</div>
+        <div id="block_7" class="block" @click="draw(7), false">{{ content[7] }}</div>
+        <div id="block_8" class="block" @click="draw(8), false">{{ content[8] }}</div>
 
-    <br>
-    <div class="play-area">
-      <div id="block_0" class="block" @click="draw(0), false">{{ content[0] }}</div>
-      <div id="block_1" class="block" @click="draw(1), false">{{ content[1] }}</div>
-      <div id="block_2" class="block" @click="draw(2), false">{{ content[2] }}</div>
-      <div id="block_3" class="block" @click="draw(3), false">{{ content[3] }}</div>
-      <div id="block_4" class="block" @click="draw(4), false">{{ content[4] }}</div>
-      <div id="block_5" class="block" @click="draw(5), false">{{ content[5] }}</div>
-      <div id="block_6" class="block" @click="draw(6), false">{{ content[6] }}</div>
-      <div id="block_7" class="block" @click="draw(7), false">{{ content[7] }}</div>
-      <div id="block_8" class="block" @click="draw(8), false">{{ content[8] }}</div>
+      </div>
 
+      <h2 class ="playerReady">{{ playerReady }}</h2>
+      <h2 id="winner" v-if="gameOver">The winner is {{ winner }}</h2>
+      <h2 id="tie" v-if="isTie">The game is a tie!</h2>
+      <h2 id="boardReseted">{{ displayBoardReset }}</h2>
+      <button @click="resetBoard()" v-if="gameOver || isTie">RESET BOARD</button>
     </div>
-
-    <h2>{{ playerReady }}</h2>
-    <h2 id="winner" v-if="gameOver">The winner is {{ winner }}</h2>
-    <h2 id="tie" v-if="isTie">The game is a tie!</h2>
-    <h2 id="boardReseted">{{ displayBoardReset }}</h2>
-    <button @click="resetBoard()" v-if="gameOver || isTie">RESET BOARD</button>
-
   </div>
 </template>
 
@@ -249,7 +249,7 @@ export default {
 
       }
 
-      
+
 
 
     },
@@ -275,7 +275,7 @@ export default {
 
       if (this.player1Ready) {
 
-        this.playerReady = "other player is ready, press start!";
+        this.playerReady = "Other player is ready, press start!";
 
 
       }
@@ -336,6 +336,15 @@ export default {
   font-family: Arial, Helvetica, sans-serif;
 }
 
+#boarder {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 3px solid rgb(250, 249, 249);
+  padding: 10px;
+}
+
 .container {
   min-height: 100vh;
   display: flex;
@@ -344,11 +353,20 @@ export default {
   justify-content: center;
   background: rgb(10, 9, 9);
   color: aliceblue;
+  border: 3px solid rgb(250, 249, 249);
 }
-
+.playerReady{
+  color: #0ff30f;
+  text-shadow: 1px 1px 4px #4fafeb, 1px 1px 10px #02ff46;
+}
+#winner{
+  color: #ff3a3a;
+  text-shadow: 1px 1px 4px #4fafeb, 1px 1px 15px #f70e1d;
+}
 h1 {
   font-size: 5rem;
   margin-bottom: 0.5em;
+  text-shadow: 1px 1px 4px #4fafeb, 1px 1px 10px #28e428;
 }
 
 h2 {
@@ -362,6 +380,7 @@ h2 {
   width: 300px;
   height: 300px;
   grid-template-columns: auto auto auto;
+
 }
 
 .block {
@@ -378,7 +397,7 @@ h2 {
 
 .block:hover {
   cursor: pointer;
-  background: #0f3df3;
+  background: #0abc0a;
 }
 
 .occupied:hover {
